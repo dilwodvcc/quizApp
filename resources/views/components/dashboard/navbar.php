19f6261cb047562f9101869426b9c69a93edf733
dilwodvc <abdullokh207@example.com>
2025-01-16 21:37:31 +0500

    database add quiz

--- resources/views/components/dashboard/navbar.php
+++ resources/views/components/dashboard/navbar.php
@@ -2,7 +2,7 @@
 
 <aside class="bg-white w-64 min-h-screen flex flex-col shadow-lg transform -translate-x-full md:translate-x-0 transition-transform duration-150 ease-in fixed md:static z-30" id="sidebar">
     <div class="p-4 border-b flex items-center justify-between">
-        <a href="/"><h2 class="text-2xl font-bold text-gray-800">Quiz App</h2></a>
+        <a href="/"><h2 class="text-2xl font-bold text-yellow-800">Quiz App</h2></a>
         <div>
             <button class="md:hidden text-gray-600" onclick="document.getElementById('sidebar').classList.toggle('-translate-x-full')">
                 <i class="fas fa-close text-xl"></i>
@@ -42,7 +42,7 @@
         ]
     let sideBar = document.getElementById("sideBar");
     sideBareItem.forEach((item) => {
-        let isActive = window.location.pathname === item.url ? 'bg-gray-100' : 'hover:bg-gray-100'; ;
+        let isActive = window.location.pathname === item.uri ? 'bg-gray-200 text-yellow-800' : 'hover:bg-gray-200'; ;
         sideBar.innerHTML += `<a href="${item.uri}" class="block p-3 mb-2 text-gray-800 ${isActive} rounded-lg">
             <i class="${item.icon}"></i> ${item.name}
         </a>`;
